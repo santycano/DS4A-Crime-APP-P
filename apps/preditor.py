c24086eb1e69cf19b3fd3cb8c8dbf99992bbd604
Nicolas Galvan <nicogalvan1@hotmail.com>
2022-06-20 21:20:53 -0500

    templates for sections and style arrangements

--- apps/preditor.py
+++ apps/preditor.py
@@ -9,15 +9,67 @@ import pathlib
 from app import app
 
 layout = html.Div([
-    html.H1('General Product Sales', style={"textAlign": "center"}),
+    # html.H1('General Product Sales', style={"textAlign": "center"}),
 
     html.Div([
-        
-        html.H1('Pruebapredictor')
-    ], className='row'),
+
+        html.Div(html.Span('Crime possibilities',style={'fontFamily': 'Inter','fontStyle': 'normal', 'fontWeight': '400','fontSize': '20px', 'lineHeight': '24px', 'color': '#FFFFFF'}),className='CrimeCounterTitle'),
+        html.Div([
+            html.Img(src=app.get_asset_url('imagenes/ExtraIcons/danger.png'),style={'padding':'5px'}),
+            html.Span('99%',className='crimePosibilitiesNumber'),
+
+        ],className='crimeCounter'),
 
 
-    
+        html.Div([
+
+            html.Div([
+                html.Span('Year',className='leftnavBarInputFont'),
+            dcc.Dropdown(['2016', '2017', '2018','2019'], id='year',style={'width':'180px'}),
+            ]),
+            
+            html.Div([
+             html.Span('Month',className='leftnavBarInputFont'),
+            dcc.Dropdown(['Janauary', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], id='Month',style={'width':'180px'}),
+
+            ],style={'paddingTop':'10px'}),
+
+            html.Div([
+             html.Span('Day',className='leftnavBarInputFont'),
+            dcc.Dropdown(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'], id='Day',style={'width':'180px'}),    
+            # dcc.Input(
+            # id="day",
+            # type="number",
+            # placeholder="",
+            # style={'height':'28px'},
+            # )            
+            ],style={'paddingTop':'10px'}),
+
+
+            html.Div([
+             html.Span('Hour',className='leftnavBarInputFont'),
+            dcc.Dropdown(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'], id='Hour',style={'width':'180px'}),              
+            ],style={'paddingTop':'10px'}),
+
+
+
+            html.Div([
+             html.Span("Neighborhood",className='leftnavBarInputFont'),
+            dcc.Dropdown(['San Francisco', 'La Concordia', 'Provenza'], id='neighborhood',style={'width':'180px'}),                
+            ],style={'paddingTop':'10px'}),
+       
+        ],style={'top':'300px','position':'absolute','paddingLeft':'28px'}),
+
+
+    ],className='leftnavBar'),
+
+    html.Div([
+    html.H2('Crime predictor'),
+
+    ],className='TituloSecciones'),
+
+
+    # dcc.Graph(id='my-map', figure={}),
 ])
 
 
--- apps/statistics.py
+++ apps/statistics.py
@@ -1,4 +1,5 @@
 # import dash_core_components as dcc
+from click import style
 from dash import dcc
 # import dash_html_components as html
 from dash import html
@@ -19,34 +20,74 @@ layout = html.Div([
     # html.H1('General Product Sales', style={"textAlign": "center"}),
 
     html.Div([
+
         html.Div(html.Span('Crime counter',style={'fontFamily': 'Inter','fontStyle': 'normal', 'fontWeight': '400','fontSize': '20px', 'lineHeight': '24px', 'color': '#FFFFFF'}),className='CrimeCounterTitle'),
         html.Div([
+            html.Span('11.530',className='crimeCounterNumber'),
+            html.Img(src=app.get_asset_url('imagenes/ExtraIcons/IconStatistics.png'),style={'padding':'5px'})
 
         ],className='crimeCounter'),
 
 
+        html.Div([
+
+            html.Div([
+                html.Span('Year',className='leftnavBarInputFont'),
+            dcc.Dropdown(['2016', '2017', '2018','2019'], id='year',style={'width':'180px'}),
+            ]),
+            
+            html.Div([
+             html.Span('Month',className='leftnavBarInputFont'),
+            dcc.Dropdown(['Janauary', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], id='Month',style={'width':'180px'}),
+
+            ],style={'paddingTop':'10px'}),
+
+            html.Div([
+             html.Span('Day',className='leftnavBarInputFont'),
+            dcc.Dropdown(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'], id='Day',style={'width':'180px'}),    
+            # dcc.Input(
+            # id="day",
+            # type="number",
+            # placeholder="",
+            # style={'height':'28px'},
+            # )            
+            ],style={'paddingTop':'10px'}),
+
+            html.Div([
+             html.Span("Victim's gender",className='leftnavBarInputFont'),
+            dcc.Dropdown(['Male', 'Female', 'Undefined'], id='gender',style={'width':'180px'}),                
+            ],style={'paddingTop':'10px'}),
+
+            html.Div([
+             html.Span("Neighborhood",className='leftnavBarInputFont'),
+            dcc.Dropdown(['San Francisco', 'La Concordia', 'Provenza'], id='neighborhood',style={'width':'180px'}),                
+            ],style={'paddingTop':'10px'}),
+
+            html.Div([
+             html.Span("Criminal offense",className='leftnavBarInputFont'),
+            dcc.Dropdown(['Robbery', 'Aggravated assault', 'Total violent crime'], id='offense',style={'width':'180px'}),                
+            ],style={'paddingTop':'10px'}),
+
+            html.Div([
+             html.Span("Victim's age",className='leftnavBarInputFont'),
+            # dcc.Input(
+            # id="age",
+            # type='range',
+            # )
+            dcc.RangeSlider(0, 80, value=[0, 80],tooltip={"placement": "bottom", "always_visible": True})
+            ],style={'paddingTop':'10px'}),
+        
+        
+        ],style={'top':'300px','position':'absolute','paddingLeft':'28px'}),
 
 
-
-        # html.Div([
-        #     html.Pre(children="Payment type", style={"fontSize":"150%"}),
-        #     dcc.Dropdown(
-        #         id='pymnt-dropdown', value='DEBIT', clearable=False,
-        #         persistence=True, persistence_type='session',
-        #         options=[{'label': x, 'value': x} for x in sorted(dfg["Type"].unique())]
-        #     )
-        # ], className='six columns'),
-
-        # html.Div([
-        #     html.Pre(children="Country of destination", style={"fontSize": "150%"}),
-        #     dcc.Dropdown(
-        #         id='country-dropdown', value='India', clearable=False,
-        #         persistence=True, persistence_type='local',
-        #         options=[{'label': x, 'value': x} for x in sorted(dfg["Order Country"].unique())]
-        #     )
-        #     ], className='six columns'),
-        # html.H1('Pruebaa')
     ],className='leftnavBar'),
+
+    html.Div([
+    html.H2('Crime statistics'),
+
+    ],className='TituloSecciones'),
+
 
     # dcc.Graph(id='my-map', figure={}),
 ])
