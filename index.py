6885bbb56e5336f751f4dc3a5448b33d84cf4589
Nicolas Galvan <nicogalvan1@hotmail.com>
2022-06-21 00:36:51 -0500

    turtle remove

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 # import dash_core_components as dcc
-from turtle import width
+# from turtle import width
 from dash import dcc
-import dash_bootstrap_components as dbc
+# import dash_bootstrap_components as dbc
 # import dash_html_components as html
 from dash import html
 from dash.dependencies import Input, Output
